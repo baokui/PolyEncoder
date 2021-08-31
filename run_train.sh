8b28ade2f0b524eeb20a9c50841f9c3ceb1bfc3f
apple-guobk <baokui2011@163.COM>
2021-08-31 19:43:17 +0800

    run_train

--- run_train.sh
+++ run_train.sh
@@ -1,3 +1,4 @@
+trainIdx=0
 python -u train1.py \
     --bert_model /search/odin/guobk/data/model/bert-base-chinese \
     --output_dir /search/odin/guobk/data/data_polyEncode/vpa/model \
@@ -5,5 +6,16 @@ python -u train1.py \
     --use_pretrain \
     --architecture poly \
     --num_train_epochs 1 \
-    --trainIdx 0 \
-    --poly_m 16 >> log/train.log 2>&1 &
+    --trainIdx $trainIdx \
+    --poly_m 16 >> log/train-$trainIdx.log 2>&1 &
+
+trainIdx=0
+python -u train1.py \
+    --bert_model /search/odin/guobk/data/model/bert-base-chinese \
+    --output_dir /search/odin/guobk/data/data_polyEncode/vpa/model \
+    --train_dir /search/odin/guobk/data/data_polyEncode/ubuntu_data \
+    --use_pretrain \
+    --architecture poly \
+    --num_train_epochs 1 \
+    --trainIdx $trainIdx \
+    --poly_m 16 >> log/train-$trainIdx.log 2>&1 &
