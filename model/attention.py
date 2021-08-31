cad37d9c1ed67a7de6f4eb9fa20e41c58d982990
apple-guobk <baokui2011@163.COM>
2021-08-31 19:40:07 +0800

    bug

--- model/attention.py
+++ model/attention.py
@@ -9,7 +9,7 @@ def dot_attention(q, k, v, v_mask=None, dropout=None):
     extended_v_mask = (1.0 - v_mask.unsqueeze(1)) * -100000.0
     print("TEST:",attention_weights.shape,extended_v_mask.shape)
     print(type(attention_weights),type(extended_v_mask))
-    attention_weights += extended_v_mask
+    attention_weights += extended_v_mask.float()
   attention_weights = F.softmax(attention_weights, -1)
   if dropout is not None:
     attention_weights = dropout(attention_weights)
