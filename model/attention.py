6f83bbfd7c428cc9fd199e89838552277482c8c7
apple-guobk <baokui2011@163.COM>
2021-09-02 10:01:28 +0800

    quxiao test

--- model/attention.py
+++ model/attention.py
@@ -7,8 +7,8 @@ def dot_attention(q, k, v, v_mask=None, dropout=None):
   attention_weights = torch.matmul(q, k.transpose(-1, -2))
   if v_mask is not None:
     extended_v_mask = (1.0 - v_mask.unsqueeze(1)) * -100000.0
-    print("TEST:",attention_weights.shape,extended_v_mask.shape)
-    print(type(attention_weights),type(extended_v_mask))
+    #print("TEST:",attention_weights.shape,extended_v_mask.shape)
+    #print(type(attention_weights),type(extended_v_mask))
     attention_weights += extended_v_mask.float()
   attention_weights = F.softmax(attention_weights, -1)
   if dropout is not None:
