6e80f16e7efb2446d3bf354cff6f9a987ed61c9f
apple-guobk <baokui2011@163.COM>
2021-09-03 12:08:09 +0800

    test

--- test.py
+++ test.py
@@ -306,28 +306,20 @@ if __name__ == "__main__":
         model, optimizer = amp.initialize(
             model, optimizer, opt_level=args.fp16_opt_level
         )
-    for step, batch in enumerate(train_dataloader, start=1):
+    step = 0
+    batch = next(train_dataloader)
+    if batch:
         #model.train()
         #optimizer.zero_grad()
         batch = tuple(t.to(device) for t in batch)
         (
             context_token_ids_list_batch,
             context_segment_ids_list_batch,
-            context_input_masks_list_batch,
             response_token_ids_list_batch,
             response_segment_ids_list_batch,
-            response_input_masks_list_batch,
-            labels_batch,
         ) = batch
-        loss = model(
-            context_token_ids_list_batch,
-            context_segment_ids_list_batch,
-            context_input_masks_list_batch,
-            response_token_ids_list_batch,
-            response_segment_ids_list_batch,
-            response_input_masks_list_batch,
-            labels_batch,
-        )
+        context_input_masks_list_batch = None
+        response_input_masks_list_batch = None
         sim = model(
             context_token_ids_list_batch,
             context_segment_ids_list_batch,
@@ -336,6 +328,6 @@ if __name__ == "__main__":
             response_segment_ids_list_batch,
             response_input_masks_list_batch,
         )
-        print("TEST",step,loss,sim)
+        print("TEST",step,sim)
             
 
