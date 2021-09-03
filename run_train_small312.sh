b9f37c049446dee74b612fb52de3633065b5dade
apple-guobk <baokui2011@163.COM>
2021-09-03 15:47:05 +0800

    train-312

--- run_train_small312.sh
+++ run_train_small312.sh
@@ -1,30 +1,33 @@
-export CUDA_VISIBLE_DEVICES="1"
-trainIdx=0
-mkdir /search/odin/guobk/data/data_polyEncode/vpa/model_small312
+# export CUDA_VISIBLE_DEVICES="1"
+# trainIdx=0
+# mkdir /search/odin/guobk/data/data_polyEncode/vpa/model_small312
+# python -u train2.py \
+#     --device 0 \
+#     --bert_model /search/odin/guobk/data/model/bert-base-chinese_small \
+#     --output_dir /search/odin/guobk/data/data_polyEncode/vpa/model_small312 \
+#     --train_dir /search/odin/guobk/data/data_polyEncode/vpa/ \
+#     --use_pretrain \
+#     --architecture poly \
+#     --num_train_epochs 1 \
+#     --trainIdx $trainIdx \
+#     --vec_dim 312 \
+#     --poly_m 16 >> log/train-small312-$trainIdx.log 2>&1 &
+
+for((epoch=0;epoch<5;epoch++))
+do
+for((i=1;i<10;i++))
+do
+trainIdx=$i
 python -u train2.py \
     --device 0 \
-    --bert_model /search/odin/guobk/data/model/bert-base-chinese_small \
+    --bert_model /search/odin/guobk/data/data_polyEncode/vpa/model_small312 \
     --output_dir /search/odin/guobk/data/data_polyEncode/vpa/model_small312 \
     --train_dir /search/odin/guobk/data/data_polyEncode/vpa/ \
     --use_pretrain \
     --architecture poly \
     --num_train_epochs 1 \
     --trainIdx $trainIdx \
-    --vec_dim 312
-    --poly_m 16 >> log/train-small312-$trainIdx.log 2>&1
-
-for((i=0;i<10;i++))
-do
-trainIdx=$i
-python -u train2.py \
-    --device 0,1 \
-    --bert_model /search/odin/guobk/data/model/bert-base-chinese_small \
-    --output_dir /search/odin/guobk/data/data_polyEncode/vpa/model_small \
-    --train_dir /search/odin/guobk/data/data_polyEncode/vpa/ \
-    --use_pretrain \
-    --architecture poly \
-    --num_train_epochs 1 \
-    --trainIdx $trainIdx \
-    --vec_dim 312
-    --poly_m 16 >> log/train-small-$trainIdx.log 2>&1
+    --vec_dim 312 \
+    --poly_m 16 >> log/train-small312-$epoch-$trainIdx.log 2>&1
+done
 done
