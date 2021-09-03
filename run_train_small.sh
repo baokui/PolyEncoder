73dbee27028e3a3cb7acc7d0d3b411e2d02c81a8
apple-guobk <baokui2011@163.COM>
2021-09-03 16:28:48 +0800

    test

--- run_train_small.sh
+++ run_train_small.sh
@@ -1,11 +1,11 @@
-export CUDA_VISIBLE_DEVICES="0,1"
+export CUDA_VISIBLE_DEVICES="0"
 for((epoch=0;epoch<3;epoch++))
 do
 for((i=0;i<10;i++))
 do
 trainIdx=$i
 python -u train2.py \
-    --device 0,1 \
+    --device 0 \
     --bert_model /search/odin/guobk/data/data_polyEncode/vpa/model_small \
     --output_dir /search/odin/guobk/data/data_polyEncode/vpa/model_small \
     --train_dir /search/odin/guobk/data/data_polyEncode/vpa/ \
@@ -13,6 +13,6 @@ python -u train2.py \
     --architecture poly \
     --num_train_epochs 1 \
     --trainIdx $trainIdx \
-    --poly_m 16 >> log/train-small-$epcoh-$trainIdx.log 2>&1
+    --poly_m 16 >> log/train-small-$epoch-$trainIdx.log 2>&1
 done
 done
